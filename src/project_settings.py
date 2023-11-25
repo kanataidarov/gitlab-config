1e290f24a1099d6067676fa474552238c068154d
Kanat Aidarov <kaidarov@beeline.kz>
2023-11-25 12:33:03 +0600

    __update_protected_branch method added

--- src/project_settings.py
+++ src/project_settings.py
@@ -1,4 +1,5 @@
 import global_utils
+import json
 import requests
 
 class ProjectSettings:
@@ -14,111 +15,6 @@ class ProjectSettings:
         self.printer = printer
 
     
-    def update_approval_settings(self, selected_pids): 
-        """Updates Approval Settings subsection (in General section) for specified GitLab Ids. 
-        :args Command Line arguments, includes defaults of optional arguments. 
-        :selected_pids List of GitLab Ids of projects belonging to specified GitLab Groups. 
-        """
-
-        for project_id in selected_pids: 
-            approvals_url = f"{self.args['base_url']}/projects/{project_id}/approvals"
-
-            response = requests.post(approvals_url, headers=self.args["headers"], data=self.args["approval_settings"])
-            
-            self.printer.dump_response(response, project_id, "Approval settings", {201})
-
-
-    def update_approval_rules(self, selected_pids): 
-        """Updates Approval Rules subsection (in General section) for specified GitLab Ids. 
-        :args Command Line arguments, includes defaults of optional arguments. 
-        :selected_pids List of GitLab Ids of projects belonging to specified GitLab Groups. 
-        """
-
-        for project_id in selected_pids: 
-            approval_rules_url = f"{self.args['base_url']}/projects/{project_id}/approval_rules"
-
-            response = requests.get(approval_rules_url, headers=self.args["headers"])
-            default_rule = [entry for entry in response.json() if entry["rule_type"]=="any_approver"]
-            if len(default_rule) == 1: 
-                self.args["approval_rules"]["id"] = default_rule[0]["id"]
-                response = requests.put(f'{approval_rules_url}/{self.args["approval_rules"]["id"]}', headers=self.args["headers"], data=self.args["approval_rules"])
-            elif len(default_rule) == 0: 
-                response = requests.post(approval_rules_url, headers=self.args["headers"], data=self.args["approval_rules"])
-            else: 
-                raise Exception(f"Project {project_id} cannot contain more than 1 default approval rule")
-            
-            self.printer.dump_response(response, project_id, "Approval rules", {200, 201})
-
-
-    def update_protected_branches(self, selected_pids):
-        """Updates Protected Branches subsection (in General section) for specified GitLab Ids. 
-        :args Command Line arguments, includes defaults of optional arguments. 
-        :selected_pids List of GitLab Ids of projects belonging to specified GitLab Groups. 
-        """
-        for project_id in selected_pids: 
-            branches_url = f"{self.args['base_url']}/projects/{project_id}/repository/branches"
-            protected_branches_url = f"{self.args['base_url']}/projects/{project_id}/protected_branches"
-
-            response = requests.get(branches_url, headers=self.args["headers"])
-            branches = response.json()
-
-            response = requests.get(protected_branches_url, headers=self.args["headers"])
-            protected_branches = response.json()
-            # print(protected_branches)
-
-            for candidate in self.args["protected_branches"]:
-                # if candidate branch exists and its not protected add it to protected branches
-                if len( [branch for branch in branches if branch["name"] == candidate["name"]] ) == 1 \
-                        and len( [branch for branch in protected_branches if branch["name"] == candidate["name"]] ) == 0:
-                    protected_branch_url = f'{protected_branches_url}?name={candidate["name"]}\
-                        &push_access_level={candidate["push_access_levels"][0]["access_level"]}\
-                        &merge_access_level={candidate["merge_access_levels"][0]["access_level"]}\
-                        &allow_force_push={candidate["allow_force_push"]}\
-                        &code_owner_approval_required={candidate["code_owner_approval_required"]}'
-                    response = requests.post(protected_branch_url, headers=self.args["headers"], data=candidate)
-                    self.printer.dump_response(response, project_id, "Protected branches", {201})
-                # if candidate branch exists and it is protected update its settings
-                if len( [branch for branch in branches if branch["name"] == candidate["name"]] ) == 1 \
-                        and len( [branch for branch in protected_branches if branch["name"] == candidate["name"]] ) == 1:
-                    
-                    self.__clear_all_access_levels(project_id, candidate["name"], protected_branches)
-
-    def __clear_all_access_levels(self, project_id, branch_name, protected_branches):
-        """Removes all access_level records for a given protected branch.
-        :project_id         Id of the project whose branch to update for.
-        :protected_branches List of protected branch objects. 
-        :branch_name        Name of the branch to remove all access levels.
-        """
-        protected_branch_url = f"{self.args['base_url']}/projects/{project_id}/protected_branches/{branch_name}"
-        candidate_branch = [branch for branch in protected_branches if branch["name"] == branch_name][0]
-
-        data = '{'
-
-        if candidate_branch["push_access_levels"]:
-            allowed_to_push_removals = "["
-            for level in candidate_branch["push_access_levels"]:
-                allowed_to_push_removals += '{{"id": {level_id}, "_destroy": true}},'.format(level_id=level["id"])
-            allowed_to_push_removals = allowed_to_push_removals[:-1] + "]"
-            data += '"allowed_to_push": ' + allowed_to_push_removals
-
-        if candidate_branch["merge_access_levels"]:
-            data += ', '
-            allowed_to_merge_removals = "["
-            for level in candidate_branch["merge_access_levels"]:
-                allowed_to_merge_removals += '{{"id": {level_id}, "_destroy": true}},'.format(level_id=level["id"])
-            allowed_to_merge_removals = allowed_to_merge_removals[:-1] + "]"
-            data += '"allowed_to_merge": ' + allowed_to_merge_removals
-
-        data += '}'
-                            
-        response = requests.patch(protected_branch_url, headers=self.args["headers"], data=data)
-        
-        if response.status_code != 200: 
-            global_utils.fail(project_id, response)
-       
-        return response
-    
-
     def update_project_settings(self, selected_pids):
         """Updates overall Project Settings for specified GitLab Ids. 
         :args Command Line arguments, includes defaults of optional arguments. 
@@ -134,8 +30,147 @@ class ProjectSettings:
                     args["project_settings"]["default_branch"] = self.default_branch
             """
 
-            project_settings_url = f"{self.args['base_url']}/projects/{project_id}"
+            project_settings_url = f'{self.args["base_url"]}/projects/{project_id}'
             response = requests.put(project_settings_url, headers=self.args["headers"], data=self.args["project_settings"])
 
-            self.printer.dump_response(response, project_id, "Project settings", {200})
+            self.printer.dump_response(response, project_id, 'Project settings', {200})
+
+
+    def update_approval_settings(self, selected_pids): 
+        """Updates Approval Settings subsection (in General section) for specified GitLab Ids. 
+        :args Command Line arguments, includes defaults of optional arguments. 
+        :selected_pids List of GitLab Ids of projects belonging to specified GitLab Groups. 
+        """
+
+        for project_id in selected_pids: 
+            approvals_url = f'{self.args["base_url"]}/projects/{project_id}/approvals'
+
+            response = requests.post(approvals_url, headers=self.args["headers"], data=self.args["approval_settings"])
+            
+            self.printer.dump_response(response, project_id, 'Approval settings', {201})
+
+
+    def update_approval_rules(self, selected_pids): 
+        """Updates Approval Rules subsection (in General section) for specified GitLab Ids. 
+        :args Command Line arguments, includes defaults of optional arguments. 
+        :selected_pids List of GitLab Ids of projects belonging to specified GitLab Groups. 
+        """
+
+        for project_id in selected_pids: 
+            approval_rules_url = f'{self.args["base_url"]}/projects/{project_id}/approval_rules'
+
+            response = requests.get(approval_rules_url, headers=self.args["headers"])
+            default_rule = [entry for entry in response.json() if entry["rule_type"]=="any_approver"]
+            if len(default_rule) == 1: 
+                self.args["approval_rules"]["id"] = default_rule[0]["id"]
+                response = requests.put(f'{approval_rules_url}/{self.args["approval_rules"]["id"]}', \
+                                        headers=self.args["headers"], data=self.args["approval_rules"])
+            elif len(default_rule) == 0: 
+                response = requests.post(approval_rules_url, headers=self.args["headers"], data=self.args["approval_rules"])
+            else: 
+                raise Exception(f'Project {project_id} cannot contain more than 1 default approval rule')
+            
+            self.printer.dump_response(response, project_id, 'Approval rules', {200, 201})
+
+
+    def update_protected_branches(self, selected_pids):
+        """Updates Protected Branches subsection (in General section) for specified GitLab Ids. 
+        :args Command Line arguments, includes defaults of optional arguments. 
+        :selected_pids List of GitLab Ids of projects belonging to specified GitLab Groups. 
+        """
+
+        for project_id in selected_pids: 
+            branches_url = f'{self.args["base_url"]}/projects/{project_id}/repository/branches'
+            branches = requests.get(branches_url, headers=self.args["headers"]).json()
+
+            protected_branches_url = f'{self.args["base_url"]}/projects/{project_id}/protected_branches'
+            protected_branches = requests.get(protected_branches_url, headers=self.args["headers"]).json()
+
+            for candidate in self.args["protected_branches"]:
+                # if candidate branch exists and it's not protected, then add it to protected branches
+                if len( [branch for branch in branches if branch["name"] == candidate["name"]] ) == 1 \
+                        and len( [branch for branch in protected_branches if branch["name"] == candidate["name"]] ) == 0:
+                    
+                    self.__add_branch_to_protected(project_id, candidate)
+
+                # if candidate branch exists and it's protected, then update its settings
+                if len( [branch for branch in branches if branch["name"] == candidate["name"]] ) == 1 \
+                        and len( [branch for branch in protected_branches if branch["name"] == candidate["name"]] ) == 1:
+                    
+                    self.__clear_all_access_levels(project_id, candidate["name"], protected_branches)
+                    self.__update_protected_branch(project_id, candidate)
+
+    def __add_branch_to_protected(self, project_id, candidate_branch):
+        """Adds given branch to protected branches as well setting its protection settings.
+        :project_id         Id of the project whose branch to update for.
+        :candidate_branch   Object of the branch to remove all access levels.
+        """
+
+        protected_branch_url = f'{self.args["base_url"]}/projects/{project_id}/protected_branches\
+                        ?name={candidate_branch["name"]}\
+                        &push_access_level={candidate_branch["push_access_levels"][0]["access_level"]}\
+                        &merge_access_level={candidate_branch["merge_access_levels"][0]["access_level"]}\
+                        &allow_force_push={candidate_branch["allow_force_push"]}\
+                        &code_owner_approval_required={candidate_branch["code_owner_approval_required"]}'.replace(' ', '')
+        print(protected_branch_url)
+        response = requests.post(protected_branch_url, headers=self.args["headers"])
+        self.printer.dump_response(response, project_id, "Protected branches", {201})
+
+    def __clear_all_access_levels(self, project_id, branch_name, protected_branches):
+        """Removes all access_level records for a given protected branch.
+        :project_id         Id of the project whose branch to update for.
+        :branch_name        Name of the branch to remove all access levels.
+        :protected_branches List of protected branch objects. 
+        """
+        protected_branch_url = f'{self.args["base_url"]}/projects/{project_id}/protected_branches/{branch_name}'
+        candidate_branch = [branch for branch in protected_branches if branch["name"] == branch_name][0]
+
+        data = '{'
+
+        if candidate_branch["push_access_levels"]:
+            allowed_to_push_removals = '['
+            for level in candidate_branch["push_access_levels"]:
+                allowed_to_push_removals += '{{"id": {level_id}, "_destroy": true}},'.format(level_id=level["id"])
+            allowed_to_push_removals = allowed_to_push_removals[:-1] + ']'
+            data += '"allowed_to_push": ' + allowed_to_push_removals + ','
+
+        if candidate_branch["merge_access_levels"]:
+            allowed_to_merge_removals = '['
+            for level in candidate_branch["merge_access_levels"]:
+                allowed_to_merge_removals += '{{"id": {level_id}, "_destroy": true}},'.format(level_id=level["id"])
+            allowed_to_merge_removals = allowed_to_merge_removals[:-1] + ']'
+            data += '"allowed_to_merge": ' + allowed_to_merge_removals + ','
+
+        data = data[:-1] + '}'
+                                    
+        response = requests.patch(protected_branch_url, headers=self.args["headers"], data=data)
+
+        if response.status_code != 200: 
+            global_utils.fail(project_id, response)
+       
+        return response
     
+    def __update_protected_branch(self, project_id, candidate_branch):
+        """Updates protection settings for a given branch.
+        :project_id         Id of the project whose branch to update for.
+        :candidate_branch   Object of the branch to remove all access levels.
+        """
+
+        protected_branch_url = f'{self.args["base_url"]}/projects/{project_id}/protected_branches/{candidate_branch["name"]}'
+        
+        data = '{'
+
+        if candidate_branch["push_access_levels"]:
+            data += '"allowed_to_push": ' + json.dumps(candidate_branch["push_access_levels"]) + ','
+        
+        if candidate_branch["merge_access_levels"]:
+            data += '"allowed_to_merge": ' + json.dumps(candidate_branch["merge_access_levels"]) + ','
+
+        data += '"allow_force_push": false,'
+        data += '"code_owner_approval_required": false,'
+        
+        data = data[:-1] + '}'
+
+        response = requests.patch(protected_branch_url, headers=self.args["headers"], data=data)
+        self.printer.dump_response(response, project_id, "Protected branches", {200})
+
