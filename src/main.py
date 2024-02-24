53c0f81024a14898ec39c95bb9eb15587fec6301
Kanat Aidarov <kaidarov@beeline.kz>
2024-02-24 12:33:16 +0600

    refactor for pycharm

--- src/main.py
+++ src/main.py
@@ -2,7 +2,12 @@ from args_parser import parse_args
 from gitlab_config import GitlabConfig
 
 
-def main(): 
+def debug_mode(gconf, selected_pids):
+    gconf.ps.update_protected_branches(selected_pids)
+    gconf.print_response()
+
+
+def main():
     args = parse_args()
     gconf = GitlabConfig(args)
 
@@ -10,14 +15,9 @@ def main():
 
     if args["debug"]:
         debug_mode(gconf, selected_pids)
-    else: 
+    else:
         gconf.update_settings(selected_pids)
         gconf.print_response()
-
-
-def debug_mode(gconf, selected_pids): 
-    gconf.ps.update_protected_branches(selected_pids)
-    gconf.print_response()
 
 
 if __name__ == "__main__":
